1b6fc34dffc987f70fe98fba65c9fe40d87de3e2
Harri Jäälinoja <harri.jaalinoja at helsinki.fi>
2014-10-16 18:08:16 +0300

    fix LoG3D jar file

--- getImageJPlugins.sh
+++ getImageJPlugins.sh
@@ -4,6 +4,8 @@
 mkdir tmp; cd tmp
 curl -O http://bigwww.epfl.ch/sage/soft/LoG3D/LoG3D.zip
 unzip LoG3D.zip
+mv LoG3D/* .
+rmdir LoG3D
 cp ../plugins.config.LoG3D plugins.config
 jar cvf ../plugins/LoG_3D.jar .
 cd ..
